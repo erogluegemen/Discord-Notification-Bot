3f858f6e1c6d67f99fe6afc831189bb102d17c18
Egemen <30879498+erogluegemen@users.noreply.github.com>
2022-03-29 23:26:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+import discord
+from discord.utils import get
+from discord.ext.commands import dm_only
+from discord.ext import commands
+
+load_dotenv()
+
+intents= discord.Intents.all()
+client = discord.Client(intents=intents)
+
+word_list = ["IEEE", "CS"]
+
+
+@client.event
+async def on_ready():
+    print("Bot hazır!")
+
+@commands.dm_only()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content in word_list:
+        id = client.get_user() # Discord ID buraya gelecek. (int formatında)
+        await id.send(content=f"Hey! {message.author} adlı kullanıcı {message.content} hakkında bir şeyler dedi.")
+
+token = os.getenv("TOKEN")
+client.run(token)
